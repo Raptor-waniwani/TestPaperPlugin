6bc7352c10d123f72832e06eff5b6dd673f98073
Raptor-waniwani <zenoanddoitu@gmail.com>
2024-11-21 01:12:13 +0900

    test

---
+++ src/main/kotlin/com/github/raptorwaniwani/testPaperPlugin/TestPaperPlugin.kt
@@ -0,0 +1,14 @@
+package com.github.raptorwaniwani.testPaperPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TestPaperPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
